a9a913fd2e72ed9c0dc1df588e03d5e67dfc8bda
Darron Park <darron1217@gmail.com>
2023-07-27 22:19:50 +0900

    chore: fix deploy & start

--- flashbots-mev-flood/deploy.sh
+++ flashbots-mev-flood/deploy.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
+IMAGE_NAME="mevflood"
+
+if [[ "$(docker images -q $IMAGE_NAME 2>/dev/null)" == "" ]]; then
+    echo "Image '$IMAGE_NAME' not found. Building the image..."
+    
+    git clone https://github.com/darron1217/mev-flood.git
+    cd mev-flood/ && git checkout devnet
+    docker build -t $IMAGE_NAME:latest .
+    cd ..
+    rm -rf mev-flood/
+fi
+
 if [ ! -f "deployments/local.json" ]; then
-    docker run -v ${PWD}/deployments:/app/cli/deployments mevflood init -r http://localhost:8545 -s local.json
+    docker run -v ${PWD}/deployments:/app/cli/deployments --network flashbots-devnet_default $IMAGE_NAME init -r http://geth:8545 -s local.json
 else
     echo "Local deployment already exists"
 fi
--- flashbots-mev-flood/start.sh
+++ flashbots-mev-flood/start.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-IMAGE_NAME="mevflood"
-
-if [[ "$(docker images -q $IMAGE_NAME 2>/dev/null)" == "" ]]; then
-    echo "Image '$IMAGE_NAME' not found. Building the image..."
-    
-    git clone https://github.com/darron1217/mev-flood.git
-    cd mev-flood/ && git checkout devnet
-    docker build -t $IMAGE_NAME:latest .
-    cd ..
-    rm -rf mev-flood/
-fi
-
 running="$(docker compose ps --services --filter "status=running")"
 if [ -n "$running" ]; then
     docker compose down
